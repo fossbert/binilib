ffa8190932c13896f815616d666ed279f9f8077d
Carlo Maurer <h.carlo.maurer@gmail.com>
2017-08-17 14:19:16 -0400

    Simplified function

--- R/plot_env.R
+++ R/plot_env.R
@@ -1,17 +1,13 @@
-
-
 #' Quick changes to the plotting environment
 #'
 #' @param a number of rows (passed to mfrow)
 #' @param b number of columns (passed to mfrow)
 #' @param brewer_n number of colours to retrieve from RColorBrewer palette
-#' @param
-mypar <- function(a = 1, b = 1, brewer_n = 9, brewer_name = "Set1",
-                  cex.lab = 1, cex.main = 1.2, cex.axis = 1,
-                  mar = c(2.5, 2.5, 1.6, 1.1), mgp = c(1.5, 0.5, 0), ...) {
+#' @param brewer_name RColorBrewer palette
+#' @param ... further arguments passed to par
+#' @return By default only a new colour palette
+mypar <- function(a = 1, b = 1, brewer_n = 9, brewer_name = "Set1", ...) {
 
-    par(mar = mar, mgp = mgp, cex.lab = cex.lab, cex.main = cex.main,
-        cex.axis = cex.axis)
     par(mfrow = c(a, b), ...)
     palette(RColorBrewer::brewer.pal(brewer_n, brewer_name))
 
