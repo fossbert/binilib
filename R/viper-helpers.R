5c268021b619ff95e761134e5787ee82a5d6337d
Carlo Maurer <h.carlo.maurer@gmail.com>
2017-11-09 16:48:18 -0500

    nullmodel for signature using limma

--- R/viper-helpers.R
+++ R/viper-helpers.R
@@ -51,9 +51,78 @@ reg2tibble <- function(reg, annotate = TRUE) {
             tmp2 <- tibble::tibble(target = unlist(regulators, use.names = FALSE),
                                    type = rep(names(regulators), purrr::map_int(regulators, length)))
 
-            dplyr::left_join(tmp, tmp2, by = "target")
+            dplyr::left_join(tmp, tmp2, by = "target") %>%
+                dplyr::mutate(type = stringr::str_replace_na(type, "None"))
+
         } else tmp
 
 }
+
+#' viperSignature and nullmodel using limma
+#'
+#' @param eset normalized gene expression matrix with samples of interest
+#' @param ref normalized gene expression matrix with reference samples
+#' @param per integer, number of overall permutations
+#' @param keep_all logical, whether to return signature for all samples (incl. reference samples)
+#' @export
+
+vpsig_limma <- function(eset, ref, per = 1000, keep_all = FALSE, seed = 42, verbose = TRUE) {
+
+    set.seed(seed)
+    if(keep_all) eset <- cbind(eset, ref) # put out all samples ?
+
+    # calculate signature using moderated t-statistics
+    if (verbose) {
+        message("\nCalculating the signature for ", ncol(eset), " samples")
+        pb <- txtProgressBar(max = ncol(eset), style = 3)
+    } else pb <- NULL
+
+    signature <- vapply(1:ncol(eset), function(i, dset, refmat){
+
+            if (!is.null(pb)) setTxtProgressBar(pb, i)
+            tmp <- eBayes(lmFit(dset[,i] - refmat))
+            qnorm(tmp$p.value[,1]/2, lower.tail = FALSE) * sign(tmp$t[,1])
+
+    }, FUN.VALUE = numeric(nrow(eset)), dset = eset, refmat = ref)
+    colnames(signature) <- colnames(eset)
+
+    # calculate nullmodel
+    if (ncol(ref)<6) {
+        vpnull <- NULL
+        warning("Not enough samples to compute null model by sample permutation,
+                gene permutation will be used instead", call.=FALSE)
+    } else {
+
+        # message methods of collapsing, number of samples and date
+        if (verbose) {
+            message("\nCalculating nullmodel using bootstraps of the ", ncol(ref), " reference samples")
+            pb <- txtProgressBar(max = ncol(ref), style = 3)
+        } else pb <- NULL
+
+
+        # leave-one-out vs. bootstrap set
+
+      vpnull <- do.call(cbind, lapply(1:ncol(ref), function(i, d22, per){
+
+            if (!is.null(pb)) setTxtProgressBar(pb, i)
+
+            vapply(1:per, function(i, x, y) {
+
+                pos <- sample(ncol(y), replace=TRUE)
+                tmp <- eBayes(lmFit(x - y[ ,pos]))
+                qnorm(tmp$p.value[,1]/2, lower.tail = FALSE) * sign(tmp$t[,1])
+
+            }, FUN.VALUE = numeric(nrow(d22)), x = d22[ ,i], y = d22[ ,-i]) # vapply end
+
+        }, d22 = ref, per = ceiling(per/ncol(ref)))) # lapply end
+    }
+
+    # put together output
+    vpsig <- list(signature = signature, nullmodel = vpnull)
+    class(vpsig) <- "viperSignature"
+    return(vpsig)
+
+}
+
 
 
